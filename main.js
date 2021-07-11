dca0ddf7f32f1a016039f328a16507605b1aa4ef
oz-om <ozyoom@gmail.com>
2021-07-11 17:42:18 +0100

    upload

---
+++ main.js
@@ -0,0 +1,23 @@
+let red = document.getElementById('red'),
+green = document.getElementById('green'),
+blue = document.getElementById('blue'),
+opacity = document.getElementById('opacity'),
+result = document.getElementById('result'),
+copy = document.querySelector('.copy');
+
+function changeColor(control) {
+  control.onchange = function() {
+    result.value = `rgb(${red.value} ${green.value} ${blue.value} / ${opacity.value}%)`;
+
+    document.documentElement.style.setProperty('--bg-Color', result.value);
+  }
+}
+changeColor(red)
+changeColor(green)
+changeColor(blue)
+changeColor(opacity)
+
+copy.addEventListener('click', function() {
+  result.select();
+  document.execCommand('copy');
+})
